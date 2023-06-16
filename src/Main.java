b660b8f982676d75ad0d5728f1b039caa886a539
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-16 13:14:24 +0530

    Create-Method-Reference

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-       greet(message -> System.out.println(message));
+       greet(System.out::println);
     }
     public static void greet(Printable printer){
         printer.Print("Welcome");

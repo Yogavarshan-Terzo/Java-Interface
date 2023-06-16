b653972f77e57696c7a257a170358c62f260f47c
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-16 13:09:40 +0530

    Create-Lambda-Expression

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-       greet(new MessagePrinter());
+       greet(message -> System.out.println(message));
     }
     public static void greet(Printable printer){
         printer.Print("Welcome");
